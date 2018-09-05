7b1c02e1f364388bbce29ed3bb734ec45c4a0c6e
Ahmad Kafaei <amdkfe@gmail.com>
2018-09-05 23:47:54 +0100

    installed tachyons, initial player component created

--- src/index.js
+++ src/index.js
@@ -1,8 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import 'tachyons';
 import './index.css';
+import {Players} from './Players'
+import PlayerCard from './PlayerCard';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+<div>
+  <App />
+  <PlayerCard name={Players[0].name} team={Players[0].team} image={Players[0].image}/>
+</div>
+  , document.getElementById('root'));
 registerServiceWorker();
